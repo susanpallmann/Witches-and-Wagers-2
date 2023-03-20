9744815cb236e260301ae6fe2f0b5c94970b45bf
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2023-03-19 20:28:33 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,79 @@
-//
+// HOST
+
+// Function Generate lobby
+
+// Function Generate monster
+
+// Function Determine next player
+
+// Function Change game phase
+
+// Phases
+/*
+title
+lobbySetup
+gameStartViable
+tutorial
+game1_determineNextPlayer
+game2_generateMonster
+game3_showPlayerMonster
+game4_openBets
+game5_timerBets
+game6_closeBets
+game7_showAnonBets
+game8_closePlayerAction_1
+game9_playerActionTimer_1
+game10_closePlayerAction_1
+game11_showNamedBets
+game12_showBetItems
+game13_1_showPlayerItems
+game13_2_calcFleeAttempt
+game14_1_calculateWinner
+game14_2_fleeAnimation
+game15_showFight
+game16_1_playerVictory
+game16_2_playerDefeat
+game17_payPlayer
+game18_betPayout
+game19_displayScoreboard
+game20_checkWinner
+game_21_restartRound
+game100_closePlayerAction_2
+game101_playerActionTimer_2
+game102_closePlayerAction_2
+victory
+credits
+gameNotViable*/
+
+// Function Timer
+
+// Function Phase receiver (changes UI of host screen)
+
+// Function Move card
+
+// Function Calculate fight winner
+
+// Function Payout bets
+
+// Function Check for winner
+
+// Function Restart round
+
+// Function Determine flee success
+
+// Function Track player count
+
+// CLIENT
+// Function Phase receiver (changes UI of player screen)
+
+// Function Move card
+
+// Function Place bet
+
+// Function Better use items
+
+// Function Choose action
+
+// Function Player use items
+
+// Function Track disconnect
