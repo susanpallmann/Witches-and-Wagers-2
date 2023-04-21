ab27199759d2f47269e57d913cee5c11f72fd94f
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2023-04-21 15:56:33 -0400

    Create join.js

---
+++ join.js
@@ -0,0 +1,27 @@
+// Get a reference to the Firebase database
+const db = firebase.database();
+
+// When the user enters a room code and their preferred username and clicks "Join Game":
+const joinGame = async (roomCode, username) => {
+  try {
+    // Authenticate the user anonymously with Firebase Authentication
+    const userCredential = await firebase.auth().signInAnonymously();
+
+    // Get the user's ID token from Firebase Authentication
+    const idToken = await userCredential.user.getIdToken();
+
+    // Add the user's ID token to the "authorizedUsers" list in your Firebase database
+    const authorizedUsersRef = db.ref(`gameCodes/${roomCode}/authorizedUsers`);
+    await authorizedUsersRef.child(userCredential.user.uid).set(true);
+
+    // Add the user's username to the "Players" directory in your Firebase database
+    const playersRef = db.ref(`gameCodes/${roomCode}/Players`);
+    await playersRef.child(username).set({
+      VIP: false,
+    });
+    
+    console.log('Successfully joined game!');
+  } catch (error) {
+    console.error('Error joining game:', error);
+  }
+};
