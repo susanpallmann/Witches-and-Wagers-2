97444c7cc31b08b8105c017ad9db509695312420
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2023-04-21 16:10:10 -0400

    Update join.js

--- join.js
+++ join.js
@@ -1,59 +1,44 @@
-// Get a reference to the Firebase database
-const db = firebase.database();
-
-// When the user enters a room code and their preferred username and clicks "Join Game":
-const joinGame = async (roomCode, username) => {
-  try {
-    // Check if there are already 8 players in the game
-    const playersRef = db.ref(`${roomCode}/Players`);
-    const playersSnapshot = await playersRef.once('value');
-    const numPlayers = playersSnapshot.numChildren();
-    if (numPlayers >= 8) {
-      console.log('Error joining game: the game is already full');
-      return;
-    }
-
-    // Authenticate the user anonymously with Firebase Authentication
-    const userCredential = await firebase.auth().signInAnonymously();
-
-    // Get the user's ID token from Firebase Authentication
-    const idToken = await userCredential.user.getIdToken();
-
-    // Add the user's ID token to the "authorized" list in your Firebase database
-    const authorizedUsersRef = db.ref(`${roomCode}/authorized`);
-    await authorizedUsersRef.child(userCredential.user.uid).set(true);
-
-    // Add the user's username to the "Players" directory in your Firebase database
-    await playersRef.child(username).set({
-      VIP: false,
-    });
+$(document).ready(function() {
+  // Get a reference to the Firebase database
+  const db = firebase.database();
+  
+  // When the form is submitted
+  $('#join-game-form').submit(async function(event) {
+    event.preventDefault(); // Prevent the form from submitting
     
-    console.log('Successfully joined game!');
-  } catch (error) {
-    console.error('Error joining game:', error);
-  }
-};
+    const roomCode = $('#room-code-input').val();
+    const username = $('#username-input').val();
+    
+    try {
+      // Authenticate the user anonymously with Firebase Authentication
+      const userCredential = await firebase.auth().signInAnonymously();
+      
+      // Get the user's ID token from Firebase Authentication
+      const idToken = await userCredential.user.getIdToken();
+      
+      // Check if the room has less than 8 players
+      const playersRef = db.ref(`gameCodes/${roomCode}/Players`);
+      playersRef.once('value', function(snapshot) {
+        const playerCount = snapshot.numChildren();
+        if (playerCount >= 8) {
+          console.log('The game is already full');
+          return;
+        }
+        
+        // Add the user's ID token to the "authorizedUsers" list in your Firebase database
+        const authorizedUsersRef = db.ref(`gameCodes/${roomCode}/authorizedUsers`);
+        await authorizedUsersRef.child(userCredential.user.uid).set(true);
 
-const joinButton = document.getElementById('joinButton');
+        // Add the user's username to the "Players" directory in your Firebase database
+        await playersRef.child(username).set({
+          VIP: false,
+        });
 
-joinButton.addEventListener('click', async (event) => {
-  event.preventDefault();
-  
-  const roomCode = document.getElementById('roomCode').value.toUpperCase();
-  const username = document.getElementById('username').value;
-  
-  try {
-    // Check if there are already 8 or more players in the game
-    const playersRef = db.ref(`${roomCode}/Players`);
-    const snapshot = await playersRef.once('value');
-    const numPlayers = snapshot.numChildren();
-    if (numPlayers >= 8) {
-      throw new Error('Cannot join game: max number of players already reached');
+        console.log('Successfully joined game!');
+      });
+      
+    } catch (error) {
+      console.error('Error joining game:', error);
     }
-
-    // Join the game
-    await joinGame(roomCode, username);
-  } catch (error) {
-    console.error(error);
-  }
+  });
 });
