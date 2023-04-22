60fb0e35cf7a0b54968911bbffb9db1f4e732c2f
Susan Pallmann <38414619+susanpallmann@users.noreply.github.com>
2023-04-21 21:21:28 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@
     }
   });
 }*/
-
+/*
 let roomCode;
 
 function updatePlayersList(roomCode) {
@@ -166,6 +166,115 @@ $(document).ready(function () {
     lobby.generate();
   });
 });
+*/
+// Define constants for the room code length and character set
+const ROOM_CODE_LENGTH = 4;
+const ROOM_CODE_CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+// Define a helper function to generate a random room code
+function generateRoomCode() {
+  let code = '';
+  for (let i = 0; i < ROOM_CODE_LENGTH; i++) {
+    code += ROOM_CODE_CHARS.charAt(Math.floor(Math.random() * ROOM_CODE_CHARS.length));
+  }
+  return code;
+}
+
+// Define a class to represent a lobby
+class Lobby {
+  constructor() {
+    this.roomCode = '';
+  }
+
+  // Generate a new room code
+  async generateRoomCode() {
+    let code = generateRoomCode();
+    while (await this.roomCodeExists(code)) {
+      code = generateRoomCode();
+    }
+    this.roomCode = code;
+    return code;
+  }
+
+  // Check if a room code exists in the database
+  async roomCodeExists(code) {
+    const snapshot = await firebase.database().ref(code).once('value');
+    return snapshot.exists();
+  }
+
+  // Create a new lobby
+  async createLobby() {
+    // Sign in anonymously
+    await firebase.auth().signInAnonymously();
+
+    // Listen for authentication state changes
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        const uid = user.uid;
+        const authorizedRef = firebase.database().ref(`${this.roomCode}/authorized/${uid}`);
+
+        // Add the current user to the list of authorized users
+        await authorizedRef.set(true);
+
+        // Set up the game controller
+        await createGameController(this.roomCode);
+
+        // Remove the user from the list of authorized users when they disconnect
+        authorizedRef.onDisconnect().set(null);
+
+        // Remove the entire room when the last user disconnects
+        firebase.database().ref(this.roomCode).onDisconnect().set(null);
+      }
+    });
+  }
+
+  // Update the list of players in the lobby
+  updatePlayersList() {
+    const playersList = $('#lobbyPlayers');
+    playersList.empty(); // Clear the list
+
+    firebase.database().ref(`${this.roomCode}/Players`).on('value', (snapshot) => {
+      snapshot.forEach((childSnapshot) => {
+        const playerName = childSnapshot.key;
+        const isVIP = childSnapshot.val().VIP;
+        const playerListItem = $('<li>').text(playerName);
+        if (isVIP) {
+          playerListItem.append(' (VIP)');
+        }
+        playersList.prepend(playerListItem);
+      });
+    });
+  }
+}
+
+// Create a new lobby object and attach an event listener to the button
+const lobby = new Lobby();
+$(document).ready(() => {
+  $('#generateLobbyButton').click(async (event) => {
+    event.preventDefault();
+    const roomCode = await lobby.generateRoomCode();
+    $('.roomCode').text(roomCode);
+    lobby.updatePlayersList();
+    await lobby.createLobby();
+  });
+});
+
+// Set up the game controller in the database
+async function createGameController(roomCode) {
+  const gameControllerRef = firebase.database().ref(`${roomCode}/gameController`);
+  await gameControllerRef.set({
+    gamePhase: 'waitingForPlayers',
+    currentPlayer: '',
+    bets: {},
+  });
+
+  // Listen for changes to the game controller
+  gameControllerRef.on('value', (snapshot) => {
+    const gameController = snapshot.val();
+    // Update the UI accordingly
+    // ...
+  });
+}
 
 // Function Generate monster
 
